3486f4f8f3db0909bab8c5de628f032ae40afd59
DarinelGuillen <darinel.escobar@outlook.com>
2025-04-25 13:07:38 -0600

    ✨ feat(destinos): add modal form and table components
    - create modal form for adding and editing destinos with fields for Código, Nombre, and Estado
    - implement table layout for displaying destinos with columns for Código, Nombre, Estado, and Acciones

---
+++ app/Views/dashboard/destinos/components/table.php
@@ -0,0 +1,11 @@
+<table id="tblDestinos" class="table table-bordered table-hover table-striped align-middle text-center shadow-sm rounded-3 overflow-hidden">
+  <thead class="table-dark">
+    <tr>
+      <th class="text-uppercase">Código</th>
+      <th class="text-uppercase">Nombre</th>
+      <th class="text-uppercase">Estado</th>
+      <th class="text-uppercase">Acciones</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider"></tbody>
+</table>
