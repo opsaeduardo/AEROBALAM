24d6470e94c958d6f32a293f586c0da520ea5d07
DarinelGuillen <darinel.escobar@outlook.com>
2025-04-25 13:07:24 -0600

    ✨ feat(routes): add destinos routes with activation support
    - introduce new routes for destinos management
    - add activate and deactivate functionalities for destinos

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -20,8 +20,12 @@ $routes->setAutoRoute(true);
 
 
 $routes->group('', ['filter' => 'auth'], static function ($routes) {
+  /* ───· MAIN VIEW HOME DASHBORAD ·─── */
+
   $routes->get('/dashboard', 'Home::dashboard');
-  
+
+  /* ───· CUPONES ·─── */
+
   $routes->get('cupones',          'CuponesController::index');
   $routes->get('cupones/list',     'CuponesController::list');
   $routes->post('cupones/store',   'CuponesController::store');
@@ -29,7 +33,19 @@ $routes->group('', ['filter' => 'auth'], static function ($routes) {
   $routes->put ('cupones/(:num)',   'CuponesController::update/$1');
   $routes->patch('cupones/(:num)',  'CuponesController::deactivate/$1');
 
+  /* ───· PERFIL ·─── */
+
   $routes->get('perfil/info', 'ProfileController::info');
   $routes->put('perfil',      'ProfileController::update');
+
+
+  /* ───· DESTINOS ·─── */
+  $routes->get   ('destinos',                 'DestinosController::index');
+  $routes->get   ('destinos/list',            'DestinosController::list');
+  $routes->post  ('destinos/store',           'DestinosController::store');
+  $routes->put   ('destinos/(:num)',          'DestinosController::update/$1');
+  $routes->patch ('destinos/(:num)',          'DestinosController::deactivate/$1');
+  $routes->patch ('destinos/activate/(:num)', 'DestinosController::activate/$1');
+
 
 });
