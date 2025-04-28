3f48251910663e159bf0180dda22482f3b200aea
DarinelGuillen <darinel.escobar@outlook.com>
2025-04-28 10:45:33 -0600

    ✨ feat(usuarios): add user management modal and table
    - create modal form for user creation and editing
    - implement table for displaying user data
    
    ✨ feat(usuarios): add user interaction scripts
    
    - implement DataTable for user list with Ajax integration
    - add user creation, editing, and activation/deactivation logic

---
+++ app/Views/dashboard/usuarios/components/table.php
@@ -0,0 +1,12 @@
+<table id="tblUsuarios" class="table table-bordered table-hover table-striped align-middle text-center shadow-sm rounded-3 overflow-hidden">
+  <thead class="table-dark">
+    <tr>
+      <th>Nombre</th>
+      <th>Usuario</th>
+      <th>Rol</th>
+      <th>Status</th>
+      <th>Acciones</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider"></tbody>
+</table>
